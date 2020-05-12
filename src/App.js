4cb76467e938dc43cdede91d103c701b62d0770b
RaiBnod <binodrayee@gmail.com>
2020-05-12 11:31:56 +0545

    Integrated react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <div>Welcome to the Rapid Platform Dashboard</div>;
+}
+
+export default Home;
