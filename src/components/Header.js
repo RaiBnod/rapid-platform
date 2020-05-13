dab0d886c4966e79ea413c9b6286e40b40bfaec7
RaiBnod <binodrayee@gmail.com>
2020-05-13 09:22:45 +0545

    - Moved logo to static file dir.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logo.png';
 
 function Header() {
   return (
     <div className="header-wrapper">
       <div className="custom-header">
         <Link to="/" className="logo">
-          <img src={logo} alt="logo" />
+          <img src="/logo.png" alt="logo" />
         </Link>
         <span className="title">Documentation</span>
       </div>
