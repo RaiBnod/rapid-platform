a854523ca79146ba91e2894060fbd840633e12da
RaiBnod <binodrayee@gmail.com>
2020-05-17 11:48:10 +0545

    - Remarkable markdown integration.

--- src/components/DisplayContent.js
+++ src/components/DisplayContent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import hljs from 'highlight.js';
+import { Remarkable } from 'remarkable';
+import ReactHtmlParser from 'react-html-parser';
 
 class DisplayContent extends Component {
   componentDidMount() {
@@ -17,11 +19,40 @@ class DisplayContent extends Component {
   };
 
   render() {
-    return (
-      <pre>
-        <code className="javascript">{this.props.page.data}</code>
-      </pre>
-    );
+    const md = new Remarkable('full', {
+      html: true, // Enable HTML tags in source
+      xhtmlOut: false, // Use '/' to close single tags (<br />)
+      breaks: false, // Convert '\n' in paragraphs into <br>
+      langPrefix: 'language-', // CSS language prefix for fenced blocks
+      // linkify: true, // autoconvert URL-like texts to links
+      linkTarget: '', // set target to open link in
+
+      // Enable some language-neutral replacements + quotes beautification
+      typographer: false,
+
+      // Double + single quotes replacement pairs, when typographer enabled,
+      // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+      quotes: '“”‘’',
+
+      // Highlighter function. Should return escaped HTML,
+      // or '' if input not changed
+      highlight(str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(lang, str).value;
+            // eslint-disable-next-line no-empty
+          } catch (__) {}
+        }
+
+        try {
+          return hljs.highlightAuto(str).value;
+          // eslint-disable-next-line no-empty
+        } catch (__) {}
+
+        return ''; // use external default escaping
+      },
+    });
+    return <>{ReactHtmlParser(md.render(this.props.page.data))}</>;
   }
 }
 
